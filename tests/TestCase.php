d76db5c49c23632f9e1f86dcf626f36385a84b56
Heiko Beineke <heiko.beineke@gmail.com>
2020-07-07 15:07:16 +0200

    Setup test database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,16 +1,16 @@
 <?php
 
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
+    use DatabaseTransactions;
+    use DatabaseMigrations;
+
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        return require __DIR__ . '/../bootstrap/app.php';
     }
 }
