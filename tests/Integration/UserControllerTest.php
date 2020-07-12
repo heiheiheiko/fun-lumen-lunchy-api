f5f06d3a52b2d75ec74b5085e192cd002e10778c
Heiko Beineke <heiko.beineke@gmail.com>
2020-07-12 22:30:56 +0200

    Revert "Wrap data around error messages"
    This reverts commit 22ded0b5e4f99d7edd626704f81a7b2953d6ca36.

--- tests/Integration/UserControllerTest.php
+++ tests/Integration/UserControllerTest.php
@@ -84,7 +84,7 @@ class UserControllerTest extends TestCase
 
         // assertions
         $this->seeStatusCode(422);
-        $this->seeJsonStructure(['data' => ['error']]);
+        $this->seeJsonStructure(['error']);
         $this->seeJsonContains(['username' => ['The username field is required.']]);
     }
 
